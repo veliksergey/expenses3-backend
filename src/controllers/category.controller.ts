69b73b31810cb53df03fc52ffd86a2afe62b34e6
Sergey V <sergey.velikoretskikh@dasi.com>
2023-11-08 22:06:16 -0500

    finishing categories

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,86 +1,122 @@
-import {Category, PrismaClient} from '@prisma/client';
+import {Category, PrismaClient, Prisma} from "@prisma/client";
 
 const categoryCtrl = new PrismaClient().category;
 
 export default class CategoryController {
-	
-	public async getList(): Promise<Category[]> {
-		
-		const list: Category[] = await categoryCtrl.findMany({
-			/*select: {
-				id: true,
-				name: true,
-				tags: true,
-				records: {
-					select: {
-						id: true,
-						name: true,
-					}
-				},
-				types: {
-					select: {
-						typeId: true,
-					}
-				}
-			},*/
-			include: {
-				types: true,
-				records: true,
-			},
-			where: {
-				id: {
-					gte: 1,
-				}
-			},
-			orderBy: {
-				name: 'asc',
-			},
-			take: 5,
-			skip: 0,
-		});
-		
-		return list;
-		
-	}
-	
-	public async getOne(id: number): Promise<Category | null> {
-		return categoryCtrl.findUnique({
-			where: {
-				id,
-			},
-			include: {
-				records: true,
-			}
-		});
-	}
-	
-	public async create(payload: Category): Promise<Category> {
-		const result = await categoryCtrl.create({
-			data: payload,
-		});
-		return result;
-	}
-	
-	public async update(id: number, payload: Category): Promise<Category> {
-		const result = await categoryCtrl.update({
-			where: {id: id},
-			data: payload,
-		});
-		return result;
-	}
-	
-	public async upsert(id: number, payload: Category): Promise<Category> {
-		return categoryCtrl.upsert({
-			where: {id},
-			update: {name: payload.name, tags: payload.tags},
-			create: {name: payload.name, tags: payload.tags},
-		});
-	}
-	
-	public async delete(id: number): Promise<Category> {
-		return categoryCtrl.delete({
-			where: {id}
-		});
-	}
-	
+
+  public async getList(): Promise<Category[]> {
+
+    const list: Category[] = await categoryCtrl.findMany({
+      /*select: {
+          id: true,
+          name: true,
+          tags: true,
+          records: {
+              select: {
+                  id: true,
+                  name: true,
+              }
+          },
+          types: {
+              select: {
+                  typeId: true,
+              }
+          }
+      },*/
+      // include: {
+      // 	types: true,
+      // 	records: true,
+      // },
+      // where: {
+      // 	id: {
+      // 		gte: 1,
+      // 	}
+      // },
+      orderBy: {
+        id: "desc",
+      },
+      // take: 5,
+      // skip: 0,
+    });
+
+    return list;
+
+  }
+
+  public async getOne(id: number): Promise<Category | null> {
+    return categoryCtrl.findUnique({
+      where: {
+        id,
+      },
+      // include: {
+      // 	records: true,
+      // }
+    });
+  }
+
+  public async create(payload: Category): Promise<Category | { message: string }> {
+    try {
+      const result = await categoryCtrl.create({
+        data: payload,
+      });
+      return result;
+    } catch (e) {
+      console.error(e);
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === "P2002") {
+          return {message: "Category with this name already exists in database"};
+        }
+      }
+      return {message: "unknown error happened"};
+    }
+  }
+
+  public async update(id: number, payload: Category): Promise<Category | {message: string}> {
+    try {
+        const result = await categoryCtrl.update({
+            where: {id: id},
+            data: payload,
+        });
+        return result;
+    } catch (e) {
+      console.error(e);
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === "P2002") {
+          return {message: "Category with this name already exists in database"};
+        }
+      }
+      return {message: "unknown error happened"};
+    }
+  }
+
+  public async upsert(id: number, payload: Category): Promise<Category | {message: string}> {
+    try {
+      return categoryCtrl.upsert({
+        where: {id},
+        update: {name: payload.name, tags: payload.tags},
+        create: {name: payload.name, tags: payload.tags},
+      });
+    } catch (e) {
+      console.error(e);
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === "P2002") {
+          return {message: "Category with this name already exists in database"};
+        }
+      }
+      return {message: "unknown error happened"};
+    }
+  }
+
+  public async delete(id: number): Promise<boolean | {message: string}> {
+    try {
+      await categoryCtrl.delete({
+        where: {id}
+      });
+      return true;
+    } catch (e) {
+      console.error(e);
+      return {message: 'Error: Could not delete category'};
+    }
+  }
+
 }
--- src/routes/category.router.ts
+++ src/routes/category.router.ts
@@ -1,26 +1,51 @@
-import express, {Request, Response} from 'express';
-import CategoryController from '../controllers/category.controller';
+import express, {Request, Response} from "express";
+import CategoryController from "../controllers/category.controller";
+import {Category} from "@prisma/client";
 
 const categoryRouter = express.Router();
 const categoryCtrl = new CategoryController();
 
-categoryRouter.get('/', async (req: Request, res: Response): Promise<Response> => {
-	const categoryList = await categoryCtrl.getList();
-	return res.status(200).json(categoryList);
+categoryRouter.get("/", async (req: Request, res: Response): Promise<Response> => {
+  const categoryList = await categoryCtrl.getList();
+  return res.status(200).json(categoryList);
 });
 
-categoryRouter.get('/:id', async (req, res) => {
-	const id: number = +req.params.id || 0;
-	const category = await categoryCtrl.getOne(id);
-	if (!category) {
-		return res.status(404).json({message: 'category not found'});
-	}
-	return res.status(200).json(category);
+categoryRouter.get("/:id", async (req, res) => {
+  const id: number = +req.params.id || 0;
+  const category = await categoryCtrl.getOne(id);
+  if (!category) {
+    return res.status(404).json({message: "category not found"});
+  }
+  return res.status(200).json(category);
 });
 
-categoryRouter.post('/', async (req: Request, res: Response) => {
-	const category = await categoryCtrl.create(req.body);
-	return res.status(200).json(category);
+categoryRouter.post("/", async (req: Request, res: Response): Promise<Response<Category>> => {
+  const createdCategory = await categoryCtrl.create(req.body);
+  return res.status(200).json(createdCategory);
+});
+
+categoryRouter.put("/:id", async (req: Request, res: Response) => {
+  const id: number = +req.params.id || 0;
+
+  // if no id provided
+  if (!id) {
+    return res.status(400).json({message: "no id was provided"});
+  }
+
+  const updatedCategory = await categoryCtrl.update(id, req.body);
+  return res.status(200).json(updatedCategory);
+});
+
+categoryRouter.delete("/", async (req: Request, res: Response) => {
+	const id = req.body.id || 0;
+  console.log(req.body);
+
+  if (!id) {
+    return res.status(400).json({message: "no id was provided"});
+  }
+
+  const deleted: boolean | {message: string} = await categoryCtrl.delete(id);
+  return res.status(200).json({deleted});
 });
 
 export default categoryRouter;
